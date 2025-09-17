2d929d6e3ee7844a70959ab7ec936957c8bf3e20
gksgPDnjs <babahhw@naver.com>
2025-09-17 23:19:05 +0900

    fix: remove external from vite.config to bundle react-icons

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,8 @@
 // vite.config.js
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // <-- 이 부분을 추가하거나 수정해 주세요!
-  build: {
-    rollupOptions: {
-      external: [
-        'react-icons',
-        'react-icons/fa',
-        'axios',
-        'react-hook-form',
-      ],
-    },
-  },
+  base: './',
 });
